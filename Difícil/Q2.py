3f5a506ab9b3b13bde2bbbebc8b72dfb73bfd8cc
juliana <user.taniajuju91@gmail.com>
2025-07-21 00:31:11 -0300

    Respostas da lista de Exercicio

--- Difícil/Q2.py
+++ Difícil/Q2.py
@@ -36,5 +36,5 @@ while tentativas > 0:
     if "_" not in adivinhadas:
         print(f"Você adivinhou a palavra '{palavraSecreta}'!")
         break
-else:
-    print(f"Suas tentativas acabaram!\nA palavra secreta era: '{palavraSecreta}'\nMais sorte na próxima vez!")
+    else:
+        print(f"Suas tentativas acabaram!\nA palavra secreta era: '{palavraSecreta}'\nMais sorte na próxima vez!")
